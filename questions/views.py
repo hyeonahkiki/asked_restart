74449e239acd8c01f0ccfdcdfbd636245c4059ce
hyoenah <kkongc7@gmail.com>
2019-10-07 10:23:33 +0900

    question create

--- questions/views.py
+++ questions/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Question
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def create(request):
+    if request.method =="POST":
+        content = request.POST.get('content')
+        Question.objects.create(content=content)
+        
+        return redirect('questions:index')
+
+    else:
+        return render(request, 'form.html')
